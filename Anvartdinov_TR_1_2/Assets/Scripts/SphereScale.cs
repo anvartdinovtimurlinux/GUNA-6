f9e552e122f9e6dfe7cf0adbc79cfa5402f61969
anvartdinovtimurlinux <39489844+anvartdinovtimurlinux@users.noreply.github.com>
2021-05-22 18:00:44 +0300

    Update SphereScale.cs

--- Anvartdinov_TR_1_2/Assets/Scripts/SphereScale.cs
+++ Anvartdinov_TR_1_2/Assets/Scripts/SphereScale.cs
@@ -4,14 +4,14 @@ public class SphereScale : MonoBehaviour
 {
     public bool isScale;
 
-    const float scaleValue = 1.5f;
+    const float SCALE_VALUE = 1.5f;
 
     // Update is called once per frame
     void Update()
     {
         if (isScale)
         {
-            transform.localScale += new Vector3(scaleValue, scaleValue, scaleValue) * Time.deltaTime;
+            transform.localScale += new Vector3(SCALE_VALUE, SCALE_VALUE, SCALE_VALUE) * Time.deltaTime;
         }
     }
 }
